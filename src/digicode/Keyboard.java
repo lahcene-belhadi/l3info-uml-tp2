bd2b6feffb4132a3906966de627ffc6b073416c9
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2021-11-05 23:37:54 +0100

    Implement getKeyPressed method

--- src/digicode/Keyboard.java
+++ src/digicode/Keyboard.java
@@ -1,17 +1,16 @@
 
 package digicode;
 
+import java.util.Scanner;
+
 // package
 import digicode.interfaces.Delayed;
-
-// lib
-import java.lang.System;
 
 
 public class Keyboard extends Delayed {
 
-    private int[] validDigits = {0, 1, 2, 3, 4, 
-                                 5, 6, 7, 8, 9};
+    private String[] validDigits = {"0", "1", "2", "3", "4", 
+                                    "5", "6", "7", "8", "9"};
     
     public Keyboard() {}
 
@@ -20,5 +19,41 @@ public class Keyboard extends Delayed {
      * 
      * @return The value the user had entered
      */
+    public int getKeyPressed() {
+
+        String input = "";
+        Scanner scan = new Scanner(System.in);
+
+        // Ask for a correct digit until timeout
+        while (!this.timedOut) {
+
+            System.out.println("Press a digit: ");
+    
+            input = scan.next();
+
+            if (this.validInput(input))
+                break;
+    
+        }
+
+        return Integer.parseInt(input);
+
+    }
+
+    /**
+     * Checks if the input is valid
+     * 
+     * @param input - The String input
+     * @return
+     */
+    private boolean validInput(String input) {
+
+        for (String digit: this.validDigits)
+            if (digit == input && input.length() == 1)
+                    return true;
+
+        return false;
+
+    }
     
 }
