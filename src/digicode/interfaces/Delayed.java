568741e4a6c67d4f372ba6a9a83e29b947a01254
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2021-11-05 23:56:24 +0100

    Add countDown method

--- src/digicode/interfaces/Delayed.java
+++ src/digicode/interfaces/Delayed.java
@@ -1,15 +1,41 @@
 
 package digicode.interfaces;
 
+// lib
+import java.lang.Thread;
+
 
 public abstract class Delayed {
     
     protected int timeout;
     protected boolean timedOut;
 
+    /**
+     * Tells that the current object is timed out
+     */
     public void timeout() {
 
         this.timedOut = true;
+
+    }
+
+    /**
+     * Starts the countdown of the object. At the end of the timer
+     * set the object to timed out.
+     */
+    public void startCountdown() {
+
+        try {
+
+            Thread.sleep(this.timeout);
+
+        } catch (Exception threadException) {
+         
+            System.out.println("Exception: " + threadException);
+        
+        }
+
+        this.timeout();
 
     }
     
