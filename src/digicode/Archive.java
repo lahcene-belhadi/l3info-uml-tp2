cd6df50e90b5a5b672ee033b07b6ce37eb57379e
Lahcène Belhadi <lahcene.belhadi@alumni.univ-avignon.fr>
2021-11-06 15:41:54 +0100

    Add Archive class and operations

---
+++ src/digicode/Archive.java
@@ -0,0 +1,68 @@
+
+package digicode;
+
+// lib
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class Archive {
+
+    /**
+     * Archives the attempt
+     * 
+     * @param attempt - The int array representing the attempt
+     */
+    public void archive(int[] attempt) {
+
+        this.createArchiveFile();
+
+        try {
+
+            // Get the archive file
+            FileWriter archive = new FileWriter("archive.txt");
+
+            // Prepare the line to write in the file
+            String strAttempt = new String();
+
+            // Add all the digit to the line
+            for (int digit: attempt) 
+                if (digit >= 0)
+                    strAttempt += digit;
+
+            // Write the line into the file
+            strAttempt += '\n';
+
+            archive.write(strAttempt);
+
+            archive.close();
+
+        } catch (IOException exception) {
+
+            System.out.println(exception);
+
+        }
+
+    }
+
+    /**
+     * Creates the archive file if it doesn't exist
+     */
+    private void createArchiveFile() {
+
+        try {
+
+            File archive = new File("archive.txt");
+
+            archive.createNewFile();
+
+        } catch (IOException exception) {
+
+            System.out.println(exception);
+
+        }
+
+    }
+    
+}
