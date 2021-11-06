595bece902d233be3a409e46c1c328a4607f6df4
Lahcène Belhadi <lahcene.belhadi@alumni.univ-avignon.fr>
2021-11-06 18:43:33 +0100

    Add Alarm

---
+++ src/digicode/Alarm.java
@@ -0,0 +1,16 @@
+
+package digicode;
+
+
+public class Alarm {
+
+    /**
+     * Rings the alarm
+     */
+    public static void ring() {
+
+        System.out.println("\n/!\\ THE DOOR IS STILL OPEN /!\\\nManual reeboot required !");
+        
+    }
+    
+}
